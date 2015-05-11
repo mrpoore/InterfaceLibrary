5431e5dcb57a11a97e30341f0bde2d7747b4c178
Richard Poore <mrrichpoore@gmail.com>
2015-05-11 15:55:41 -0500

    UI for entering Books

--- LibrarywInterface/src/Library/Book.java
+++ LibrarywInterface/src/Library/Book.java
@@ -5,6 +5,28 @@ public class Book implements Item{
 	private String callNumber;
 	private boolean checkedOut;
 	private long custID;
+	private String title;
+	private String author;
+	
+	public String getTitle()
+	{
+		return title;
+	}
+	
+	public void setTitle(String t)
+	{
+		title = t;
+	}
+	
+	public String getAuthor()
+	{
+		return author;
+	}
+	
+	public void setAuthor(String a)
+	{
+		author = a;
+	}
 	
 	@Override
 	public void setCallNumber(String callNum) {
