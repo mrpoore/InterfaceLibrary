18c71afdc3e47a6cdf6d162c948d94691b98ce9e
Richard Poore <mrrichpoore@gmail.com>
2015-05-11 12:23:33 -0500

    More Changes --- A little of main

--- LibrarywInterface/src/Library/Library.java
+++ LibrarywInterface/src/Library/Library.java
@@ -1,10 +1,33 @@
 package Library;
 
+import java.util.Scanner;
+
+
 public class Library {
+	static int minCustomers = 100;
 
+	//Create a scanner object each for text and numbers
+	private static Scanner tInput = new Scanner(System.in);
+	private static Scanner nInput = new Scanner(System.in);
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		do 
+		{
+		System.out.println("What is the maximum amount of customers you expect?");
+		} while (!(Integer.parseInt(tInput.nextLine())>=minCustomers));
+		
 
+		
+		
+	}
+	
+	private int addCustomer()
+	{
+		
+		
+		
+		return 0;
 	}
 
 }
